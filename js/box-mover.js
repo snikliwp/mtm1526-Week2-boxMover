125fd1397b04e825c6a76ba4f359a260e7ad42bc
snikliwp <wilk0146@algonquinlive.com>
2012-01-20 12:57:05 -0500

    should be fininshed

--- js/box-mover.js
+++ js/box-mover.js
@@ -15,8 +15,9 @@ var buttonDown;
 
 var createBox = function(ev) {
 // 	console.log("It is working");
+	removeActiveClass();
 	box = document.createElement("div"); // createElement() makes a new empty HTML tag
-	box.className = "box"; //className allows us to manipulate the classes of a tag
+	box.className = "box active"; //className allows us to manipulate the classes of a tag
 	document.body.appendChild(box); // append child will add a element to the HTML file
 
 	var randLeft = Math.round(Math.random() * (document.documentElement.clientWidth - box.offsetWidth));
@@ -55,9 +56,47 @@ document.getElementById("up").addEventListener("click", moveUp, false);
 document.getElementById("left").addEventListener("click", moveLeft, false);
 document.getElementById("right").addEventListener("click", moveRight, false);
 
+document.documentElement.addEventListener("keydown", function(ev) {
+	switch(ev.keyCode) {
+		case 37 : moveLeft();
+		break;
+		case 38 : moveUp();
+		break;
+		case 39 : moveRight();
+		break;
+		case 40 : moveDown();
+		break;
+		default: console.log(ev.keyCode);
+		break;
+		}
+		
+		
+}, false);
 
+document.getElementById("color").addEventListener("change",function (ev) {
+	// inside the event listener "this" refers to the element that the event listener is attached to
+	console.log(this.value);
+	
+	console.log(ev.target.value);
+	
+	box.style.backgroundColor = this.value;
+}, false);
 
+document.documentElement.addEventListener("click", function (ev) {
+	if (ev.target.tagName == "DIV") {
+		removeActiveClass();
+		ev.target.classname = "box active";
+		box = ev.target;
+	}
+	
+}, false);
 
-
-
+var removeActiveClass = function () {
+	var divs = document.getElementsByTagName("div");
+	var totalDivs = divs.length;
+	
+	for (var i =0; i < totalDivs; i++) {
+	box.className = "box";
+	}
+};
 
